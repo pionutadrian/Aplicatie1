87c50328993f8123868d6904cd78705630d5ae79
pionutadrian <ionut_adrian1992@yahoo.co.uk>
2013-11-17 17:54:53 -0500

    tema1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Aplicatie1::Application.routes.draw do
+  root to: 'static_pages#home'
+  
+  match 'about', to: 'static_pages#about', via: :get
+  match 'contact', to: 'static_pages#contact', via: :get
+  match 'home', to: 'static_pages#home', via: :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
